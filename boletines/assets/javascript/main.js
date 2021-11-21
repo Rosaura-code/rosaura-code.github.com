22014eea4e924ab49a594c9c3bf105a6c11f4f57
Rosaura Martínez <66541122+Rosaura-code@users.noreply.github.com>
2021-11-21 12:23:57 -0600

    boletines

---
+++ boletines/assets/javascript/main.js
@@ -0,0 +1,41 @@
+
+
+//DOM
+/*let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(link);
+});*/
+
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click', function(){
+        console.log(this);
+    })
+});*/
+
+//Obtener los elementos de la clase .close
+let claseclose = document.querySelectorAll(".close");
+//Recorrerlos
+claseclose.forEach(function(close){
+    //Agregar un evento click a cada uno de ellos
+    close.addEventListener("click", function(evento){
+        evento.preventDefault();
+
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+        //setInterval
+
+        return false;
+    })
+});
+
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("")
+});*/
+
