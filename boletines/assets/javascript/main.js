98bf65b2938427aad52e1e78daa5e376e35aff48
Rosaura Martínez <66541122+Rosaura-code@users.noreply.github.com>
2021-11-21 12:34:40 -0600

    Update main.js

--- boletines/assets/javascript/main.js
+++ boletines/assets/javascript/main.js
@@ -24,7 +24,7 @@ claseclose.forEach(function(close){
         evento.preventDefault();
 
         setTimeout(function(){
-            location.href = "./boletines";
+            location.href = "/boletines";
         },600);
         //setInterval
 
